809c98cc6fdeba864f26b33ad3d80cb6ffeaf7f4
Иван Филиппов <filippov@power.corp>
2025-01-04 00:22:42 +0300

    Реализация ТЗ-11

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GeneralException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class GeneralException extends RuntimeException {
+    public GeneralException(String message) {
+        super(message);
+    }
+}
